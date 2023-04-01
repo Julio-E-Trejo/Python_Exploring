c37e3cf8b24154a44dcd1a11a999ada5d68b601a
Julio-E-Trejo <rtjulio119@arizona.edu>
2023-04-01 15:30:16 -0700

    Partially resolved; render not operating correctly

--- Basics_Practice/PencilSketch.py
+++ Basics_Practice/PencilSketch.py
@@ -6,7 +6,8 @@ class PencilSketch:
         self.height = height
 
         # try to open background canvas (if it exists)
-        self.canvas = bg_gray
+        img_gray = cv2.cvtColor(bg_gray, cv2.COLOR_RGB2GRAY)
+        self.canvas = img_gray
         if self.canvas is not None:
             self.canvas = cv2.resize(self.canvas, (self.width, self.height))
 
@@ -18,12 +19,12 @@ class PencilSketch:
         img_gray_inv = 255 - img_gray
         img_blur = cv2.GaussianBlur(img_gray_inv, (21,21), 0, 0)
         img_blend = cv2.divide(img_gray, 255-img_blur, scale=256)
-        #if self.canvas is not None:
-            #img_blend = cv2.multiply(img_blend, self.canvas, scale=1./256)
         return cv2.cvtColor(img_blend, cv2.COLOR_GRAY2RGB)
     
-    def render(img_rgb):
+    def render(self, img_rgb):
         img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
         img_blur = cv2.GaussianBlur(img_gray, (21,21), 0, 0)
         img_blend = cv2.divide(img_gray, img_blur, scale=256)
+        if self.canvas is not None:
+            img_blend = cv2.multiply(img_blend, self.canvas, scale=1./256)
         return img_blend
