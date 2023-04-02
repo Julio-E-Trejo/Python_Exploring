de71f97bdf0695e2bf8bd22e706c27e88242b8fd
Julio-E-Trejo <rtjulio119@arizona.edu>
2023-04-01 18:59:15 -0700

    Set pencil filter as a function

--- Basics_Practice/camFilters.py
+++ Basics_Practice/camFilters.py
@@ -4,9 +4,14 @@ import PencilSketch as ps
 # inputs: 
 #   image -> retrieved camera frame 
 #   mask -> filter with 0 & non-0 values; all 0s set 0s in the output image
-def burnV2(image, mask):
+def burn(image, mask):
     return 255 - cv2.divide(255-image, 255-mask, scale=256)
 
+# Pencil-like appearance of the image
+def pencil(image,w,h):
+    image = cv2.resize(image, (w,h))
+    ps.PencilSketch.__init__(ps,w,h,image)
+    return ps.PencilSketch.renderV2(ps,image)
 
 
 
@@ -16,12 +21,10 @@ while(True):
     # ret: whether frames were grabbed 
     # frame: frame capture
     ret,frame = cam.read()
-    frame = cv2.resize(frame, (512,512))
-    # filtering
-    ps.PencilSketch.__init__(ps,512,512,frame)
 
     # shows the frame
-    cv2.imshow('Pencil sketch (Exit with c)',ps.PencilSketch.renderV2(ps,frame))
+    # frame is modified by corresponding function used
+    cv2.imshow('Pencil sketch (Exit with c)',pencil(frame,512,512))
 
     # change the character in ord() for a different exit key
     if cv2.waitKey(1) & 0xFF == ord('c'):
