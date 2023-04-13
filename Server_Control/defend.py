03cb1717502d4ee11be4caef47b2cd0756da4fd2
Julio-E-Trejo <rtjulio119@arizona.edu>
2023-04-12 20:31:30 -0700

    Comments and reducing y appearance

--- Server_Control/defend.py
+++ Server_Control/defend.py
@@ -1,6 +1,8 @@
 import cv2
 from grip import WhiteBall as WB
-
+# NOTE: all references to "y" are in the z-direction
+# All references to "z" are also in the z-direction
+# Movements made are in the x-z plane; y is kept constant
 class defense:
     def __init__(self):# Base initializations
         self.I = WB()
@@ -11,19 +13,46 @@ class defense:
 
         # Make equivalent to starting defense position
         self.x_prev = 0
-        self.y_prev = 0
+        self.z_prev = 0
+        self.x = 0
+        self.z = 0
 
-    def locate(self, frame):
+    def locate(self, frame):# Location establishing
+        # Runs the image frame through grip.py
         self.I.process(frame)
-        if len(self.I.ball) > 0:
-            x = self.I.ball[1] + self.I.startX
-            y = self.I.ball[2] + self.I.startY
-            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-            cv2.drawMarker(frame, (x, y),  (0, 0, 255), cv2.MARKER_CROSS, 10, 1)
-            txt = "{},{},{}".format(hsv[y,x,0], hsv[y,x,1], hsv[y,x,2])
-            cv2.putText(frame, txt, (x,y), self.font, self.fontScale, self.color, self.thickness, cv2.LINE_AA)
 
-    # mX represents linear gain in 
-    def move(self,mX,mZ):
-        
-        return ballx,bally
+        # Accessed if a "ball" is found
+        if len(self.I.ball) > 0:
+            # Making current position values
+            x = self.I.ball[1] + self.I.startX
+            z = self.I.ball[2] + self.I.startY
+
+            # Image processing to frame the ball
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            cv2.drawMarker(frame, (x, z),  (0, 0, 255), cv2.MARKER_CROSS, 10, 1)
+            txt = "{},{},{}".format(hsv[z,x,0], hsv[z,x,1], hsv[z,x,2])
+            cv2.putText(frame, txt, (x,z), self.font, self.fontScale, self.color, self.thickness, cv2.LINE_AA)
+            
+            # Setting prior & current position values
+            self.x_prev = self.x
+            self.y_prev = self.y
+            self.x = x
+            self.y = z
+
+
+    
+    def move(self):
+
+        # Initial values
+        x0 = 1
+        z0 = 1
+
+        # Pixel-to-motion relation
+        mX = 1
+        mZ = 1
+
+        # Linear relation between image pixels & meArm motion
+        ballx = mX*self.x + x0
+        ballz = mZ*self.z + z0
+
+        return [ballx,ballz]
